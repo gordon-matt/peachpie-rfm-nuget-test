8f1bd62a0294e8cedb9ab886502fa84ddbe8e344
vnmatt <gordon_matt@hotmail.com>
2018-03-23 01:24:35 +1030

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var root = Path.GetDirectoryName(Directory.GetCurrentDirectory()) + "\\ConsoleApp1\\wwwroot";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseWebRoot(root)
+                .UseContentRoot(root)
+                .UseUrls("http://*:5004/")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ ConsoleApp1/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Pchp.Core;
+using Peachpie.Web;
+
+namespace ConsoleApp1
+{
+    internal class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ResponsiveFileManagerConfig>(Configuration.GetSection("ResponsiveFileManagerConfig"));
+
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+            
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseSession();
+
+            var rfmOptions = app.ApplicationServices.GetRequiredService<IOptions<ResponsiveFileManagerConfig>>().Value;
+
+            app.UsePhp(new PhpRequestOptions(scriptAssemblyName: "ResponsiveFileManager")
+            {
+                //RootPath = Path.GetDirectoryName(Directory.GetCurrentDirectory()) + "\\Website",
+                BeforeRequest = (Context ctx) =>
+                {
+                    // Since the config.php file is compiled, we cannot modify it once deployed... everything is hard coded there.
+                    //  TODO: Place these values in appsettings.json and pass them in here to override the ones from config.php
+
+                    ctx.Globals["appsettings"] = (PhpValue)new PhpArray()
+                    {
+                        { "upload_dir", rfmOptions.UploadDirectory },
+                        { "current_path", rfmOptions.CurrentPath },
+                        { "thumbs_base_path", rfmOptions.ThumbsBasePath }
+                    };
+                }
+            });
+            
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
